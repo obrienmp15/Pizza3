c59b49815afe8cf04900b26a60dfafb5573a384b
matt obrien <obrien.matthew.cs@gmail.com>
2016-12-02 09:52:21 -0500

    Test update

---
+++ PizzaStoreMvc/PizzaStoreMvc.Business/EFData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaStoreMvc.Business
+{
+  public class EFData
+  {
+    private PizzaDBEntities db = new PizzaDBEntities();
+
+    public List<Sauce> GetSauces()
+    {
+      return db.Sauces.ToList();
+    }
+
+  }
+}
---
+++ PizzaStoreMvc/PizzaStoreMvc.Tests/EFDataTests.cs
@@ -0,0 +1,24 @@
+﻿using PizzaStoreMvc.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PizzaStoreMvc.Tests
+{
+  public class EFDataTests
+  {
+    [Fact]
+    public void Test_GetSauces()
+    {
+      var data = new EFData();
+      var actual = data.GetSauces();
+
+      Assert.Empty(actual);
+
+    }
+    
+  }
+}
